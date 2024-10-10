80353d2392f336d686ebec24230873db06582592
loexes20145 <emaklov8145@gmail.com>
2024-10-10 10:04:06 +0000

    new commit

--- js/main.js
+++ js/main.js
@@ -13,6 +13,23 @@ class Carousel {
             slideToScroll: 1,
             slideVisible: 1
         }, options)
+        this.children = [].slice.call(element.children)
+
+        let root = this.createDivWithClass('carousel')
+        let container = this.createDivWithClass('carousel__container')
+        root.appendChild(container)
+        this.element.appendChild(root)
+        this.children.forEach((child) => {
+            let item = this.createDivWithClass('carousel__item')
+            item.appendChild(child)
+            container.appendChild(item)
+        })
+    }
+
+    createDivWithClass (className) {
+        let div = document.createElement('div')
+        div.setAttribute('class', className)
+        return div
     }
 }
 
