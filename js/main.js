21635be37b6bcf9749b46ec82d9e3c31d2629675
loexes20145 <emaklov8145@gmail.com>
2024-10-10 09:26:04 +0000

    new commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+class Carousel {
+
+    /**
+     * 
+     * @param {HTMLElement} element 
+     * @param {Object} options 
+     * @param {Object} options.slideToScroll Nombre d'éléments à faire défiler
+     * @param {Object} options.slideVisible Nombre d'éléments visibles dans un slide
+     */
+    constructor (element, options = {}) {
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    new Carousel (document.querySelector('#carousel1')), {
+        slideToScroll : 3,
+        slideVisible : 3
+    }
+})
